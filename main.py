4c69c677d8f264ca7cf1e1c97581aa0c57e25d6a
K0ndrich <123kondrich@gmail.com>
2024-11-25 16:57:12 +0200

    asdasdasd

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ import requests
 link = "https://icanhazip.com/"
 
 # отправляем GET-запрос на наш сайт для получения данных
-requests.get
+
+# возвращает статус(успешность) запроса
+response = requests.get(link)  # -> <Response [200]>
+
+# .text возвращает данные с страници в виде html или json
+response = requests.get(link).text  # -> 159.224.217.57
+
+# .content возвращет данные с странци в виде байтовой строки 
+response = requests.get(link).content  # -> b'159.224.217.57\n'
